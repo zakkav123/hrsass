d2484a85735474e76339852cbca58b35bc09f2c8
赵合强 <11255995+zakkav@user.noreply.gitee.com>
2022-08-05 14:43:20 +0800

    获取组织架构数据，并进行树形处理

---
+++ src/views/departments/components/Treetools.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-row type="flex" style="width: 100%">
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex">
+        <!-- 两个内容 -->
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 下拉菜单 element -->
+          <el-dropdown>
+            <span> 操作<i class="el-icon-arrow-down" /> </span>
+            <!-- 下拉菜单 -->
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <template v-if="!isRoot">
+                <el-dropdown-item>添加子部门</el-dropdown-item>
+                <el-dropdown-item>添加子部门</el-dropdown-item>
+              </template>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  props: {
+    treeNode: {
+      type: Object,
+      required: true
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  },
+  created() {},
+
+  methods: {}
+}
+</script>
+
+<style scoped></style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,21 +1,59 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="tree-card">
+        <!-- 头部 -->
+        <Treetools
+          :isRoot="true"
+          :treeNode="{ name: '传智教育', manager: '负责人' }"
+        ></Treetools>
+        <!-- 树状结构 -->
+        <el-tree
+          :data="treeData"
+          :props="defaultProps"
+          :default-expand-all="true"
+        >
+          <template v-slot="{ data }">
+            <Treetools :treeNode="data"></Treetools>
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import Treetools from './components/Treetools.vue'
+import { getDepartments } from '@/api/departments'
+import { transListToTree } from '@/utils/index'
 export default {
   data() {
-    return {}
+    return {
+      treeData: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' }
+      ],
+      defaultProps: {
+        label: 'name'
+      }
+    }
+  },
+  components: {
+    Treetools
+  },
+  created() {
+    this.getDepartments()
   },
 
-  created() {},
-
-  methods: {}
+  methods: {
+    async getDepartments() {
+      const res = await getDepartments()
+      console.log(res)
+      this.treeData = transListToTree(res.depts, '')
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped></style>
